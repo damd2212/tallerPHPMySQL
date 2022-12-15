79f60cc5f0ab39d562faf77373090de9002ecfbb
jeffersoncy <48133501+jeffersoncy@users.noreply.github.com>
2022-12-14 22:54:43 -0500

    datos terminados

--- controlador/cntrlGeneral.php
+++ controlador/cntrlGeneral.php
@@ -2,6 +2,8 @@
 session_start();
 /*este es para redirigir un controlador*/
 if (isset($_SESSION['user'])) {
+    //redirige una peticion de acuerdo a los parametros que reciba por url y llamara al controlador especifico
+    //por ejemplo c=tracks, a=listar
     if (isset($_REQUEST['c'])) {
 
         $controlador = $_REQUEST['c'];
@@ -10,14 +12,18 @@ if (isset($_SESSION['user'])) {
         $controlador = "controlador" . $controlador;
         $controlador = new $controlador;
         call_user_func(array($controlador, $accion));
-    } else {
+    }
+    //esta es la pagina inicial por defecto 
+    else {
         $controlador = "tracks";
         require "cntrl.$controlador";
         $controlador = "controlador" . $controlador;
         $controlador = new $controlador;
         $controlador->Listar();
     }
-} else {
+}
+//redirigimos al controlador de usuario para crear una nueva sesión
+else {
     $controlador = "Customers";
     require "cntrl.$controlador";
     $controlador = "controlador" . $controlador;
--- index.php
+++ index.php
@@ -1,13 +1,16 @@
 <?php
 session_start();
+//preguntamos si inicio sesión el usuario
 if (isset($_SESSION['user'])) {
     header("Location: controlador/cntrlGeneral.php?c=tracks&a=Listar"); 
 }
 else{
+    //si intentamos ingresar con credenciales incorrectas
     if (isset($_REQUEST['mensajeLogin'])) {
         echo '<div class="alert alert-danger" id="demo"><strong>Success!</strong> Indicates a successful or positive action.</div>';
         echo '<script type="text/javascript">document.getElementById("demo").innerHTML = "Credenciales incorrectas"; const value = document.getElementById("demo"); setTimeout(() => { value.parentNode.removeChild(value); }, 5000);</script>';
     }
+    //si intentamos ingresar desde la url a una pagina sin iniciar sesión
     if(isset($_REQUEST['mensajeError'])){
         echo '<div class="alert alert-warning" id="demo"><strong>Success!</strong> Indicates a successful or positive action.</div>';
         echo '<script type="text/javascript">document.getElementById("demo").innerHTML = "Inicia sesión para continuar"; const value = document.getElementById("demo"); setTimeout(() => { value.parentNode.removeChild(value); }, 5000);</script>';
